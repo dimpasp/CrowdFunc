9bae16139a1cd352f46ac63f7e55a695058e0af1
KCA12 <kotsidisgiorgos@hotmail.gr>
2020-02-21 11:43:05 +0200

    create web

---
+++ CrowdFundWeb/Controllers/CreatorControler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CrowdFun.Core.data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrowFun.web.Controllers {
+    public class CreatorControler:Controller
+    {
+   
+    }
+}
